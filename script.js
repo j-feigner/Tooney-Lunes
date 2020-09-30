25757c3c6e79caaf7ae3d18a4abe1c5d699ebbdc
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-09-29 19:46:42 -0700

    Piano visual render

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-document.getElementById("Welcome").innerHTML = "Hello World";
+createPiano();
+
+function createPiano() {
+    var piano_container = document.getElementById("pianoBlock");
+
+    // Initialize piano key array
+    var keys = [];
+    for(var i = 0; i < 13; i++) {
+        // Create new key div element at keys[i]
+        keys.push(document.createElement("div"));
+        
+        // Check if current key is white or black
+        if(i == 1 || i == 3 || i == 6 || i == 8 || i == 10) {
+            keys[i].className = "black-key";
+        } 
+        else {
+            keys[i].className = "white-key";
+        }
+    }
+
+    // Insert piano key div elements into piano container in sequence
+    for(var i = 0; i < keys.length; i++) {
+        piano_container.appendChild(keys[i]);
+    }
+}
