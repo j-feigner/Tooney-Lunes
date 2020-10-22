4e7776e706ef04c787c5faa563d7f05021069b3f
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-10-22 15:58:04 -0700

    Added GuitarString object outline

--- src/guitar.js
+++ src/guitar.js
@@ -1,15 +1,44 @@
-var canvas = createCanvas();
-var sounds = createGuitarSoundArray();
+window.onload = main;
 
-drawStrings(canvas);
+function main() {
+    var canvas = createCanvas();
+    var canvas_context = canvas.getContext("2d");
+    
+    var sounds = createGuitarSoundArray();
+
+}
+
+function GuitarString(rect_x, rect_y, rect_w, rect_h, sounds) {
+    this.rect = {
+        x: rect_x,
+        y: rect_y,
+        width: rect_w,
+        height: rect_h
+    };
+    this.sounds = sounds;
+    this.fret = 0;
+    this.is_strumming = false;
+
+    this.drawString = function() {
+
+    };
+
+    this.pluckString = function() {
+
+    };
+
+    this.isStrummed = function() {
+
+    };
+}
 
 function createCanvas() {
     var guitar_block = document.getElementById("guitarBlock");
     var canvas = document.createElement("canvas");
     guitar_block.appendChild(canvas);
 
-    canvas.style.height = "600px";
-    canvas.style.width = "1000px";
+    canvas.height = 600;
+    canvas.width = 1000;
     canvas.style.border = "1px solid black";
 
     return canvas;
@@ -32,8 +61,4 @@ function createGuitarSoundArray() {
     }
 
     return sounds;
-}
-
-function drawStrings(canvas) {
-    canvas_context = canvas.getContext('2d');
 }
