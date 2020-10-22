b2e95c1130f9a5ffdfedd60d7a746327c900cf96
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-10-21 18:01:13 -0700

    Moved canvas creation to script for IntelliSense

--- src/guitar.js
+++ src/guitar.js
@@ -1,3 +1,39 @@
-var canvas = document.getElementById('c');
+var canvas = createCanvas();
+var sounds = createGuitarSoundArray();
 
-canvas.style.backgroundColor = "rgb(0, 0, 0)";
+drawStrings(canvas);
+
+function createCanvas() {
+    var guitar_block = document.getElementById("guitarBlock");
+    var canvas = document.createElement("canvas");
+    guitar_block.appendChild(canvas);
+
+    canvas.style.height = "600px";
+    canvas.style.width = "1000px";
+    canvas.style.border = "1px solid black";
+
+    return canvas;
+}
+
+function createGuitarSoundArray() {
+    var sound_srcs = [
+        "sound_files/guitar/1_0_E3.mp3",
+        "sound_files/guitar/2_0_A3.mp3",
+        "sound_files/guitar/3_0_D4.mp3",
+        "sound_files/guitar/4_0_G4.mp3",
+        "sound_files/guitar/5_0_B4.mp3",
+        "sound_files/guitar/6_0_E5.mp3"
+    ];
+
+    var sounds = [];
+    for(var i = 0; i < sound_srcs.length; i++) {
+        var sound = new Audio(sound_srcs[i]);
+        sounds.push(sound);
+    }
+
+    return sounds;
+}
+
+function drawStrings(canvas) {
+    canvas_context = canvas.getContext('2d');
+}
