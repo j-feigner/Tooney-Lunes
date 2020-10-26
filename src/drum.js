8fd9b39e959f5cc80bbef02737c6166b164ab679
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-10-26 14:30:23 -0700

    Added Drum object template

--- src/drum.js
+++ src/drum.js
@@ -4,10 +4,8 @@ function main() {
     var canvas = document.createElement("canvas");
     var div = document.getElementById("drumBlock");
     div.appendChild(canvas);
-
+    canvas.id = "drumCanvas";
     canvas.style.border = "1px solid black";
-
-    ctx = canvas.getContext("2d");
 }
 
 function DrumKit() {
@@ -19,4 +17,25 @@ function DrumKit() {
     this.floor_tom = null;
     this.tom1 = null;
     this.tom2 = null;
+}
+
+function Drum(sound_src, center_x, center_y, radius) {
+    this.sound = new Audio();
+    this.sound.src = sound_src;
+    this.arc = {
+        x: center_x,
+        y: center_y,
+        r: radius,
+        sAngle: 0,
+        eAngle: 2 * Math.PI
+    }
+
+    this.isInBounds = function(x, y) {
+        var coord_radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        if(coord_radius <= this.arc.r) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
