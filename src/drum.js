0d5ec3a66c9309780a52e5b1e6953cc3eee50992
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-10-26 18:29:17 -0700

    Added drum SVG images and restructured Drum object

--- src/drum.js
+++ src/drum.js
@@ -30,13 +30,13 @@ function DrumKit() {
     this.drums = [];
 
     this.createDrums = function() {
-        this.drums[0] = new Drum("kick", "sound_files/drums/kick.mp3", 400, 400, 50);
-        this.drums[1] = new Drum("snare", "sound_files/drums/snare.mp3", 465, 465, 25);
-        this.drums[2] = new Drum("hi_hat", "sound_files/drums/hi_hat.mp3", 325, 465, 25);
-        this.drums[3] = new Drum("tom1", "sound_files/drums/tom1.mp3", 375, 300, 20);
-        this.drums[4] = new Drum("tom2", "sound_files/drums/tom2.mp3", 425, 300, 20);
-        this.drums[5] = new Drum("crash", "sound_files/drums/crash.mp3", 300, 375, 35);
-        this.drums[6] = new Drum("ride",  "sound_files/drums/ride.mp3", 500, 375, 35);
+        this.drums[0] = new Drum("snare", "sound_files/drums/snare.mp3", "image_files/snare.svg", 1150, 650, 200, 200);
+        this.drums[1] = new Drum("kick", "sound_files/drums/kick.mp3", "image_files/kick.svg", 750, 500, 350, 300);
+        this.drums[2] = new Drum("tom1", "sound_files/drums/tom1.mp3", "image_files/tom.svg", 950, 400, 100, 100);
+        this.drums[3] = new Drum("tom2", "sound_files/drums/tom2.mp3", "image_files/tom.svg", 800, 400, 100, 100);
+        this.drums[4] = new Drum("hi_hat", "sound_files/drums/hi_hat.mp3", "image_files/cymbal.svg", 550, 650, 150, 150);
+        this.drums[5] = new Drum("crash", "sound_files/drums/crash.mp3", "image_files/cymbal.svg", 550, 375, 225, 225);
+        this.drums[6] = new Drum("ride",  "sound_files/drums/ride.mp3", "image_files/cymbal.svg", 1050, 375, 225, 225);
     }
 
     this.animateDrums = function() {
@@ -50,14 +50,18 @@ function DrumKit() {
     }
 }
 
-function Drum(drum_name, sound_src, center_x, center_y, radius) {
+function Drum(drum_name, sound_src, image_src, center_x, center_y, width, height) {
     this.name = drum_name;
     this.sound = new Audio();
     this.sound.src = sound_src;
+    this.img = new Image();
+    this.img.src = image_src;
+    this.img.width = width;
+    this.img.height = height;
     this.arc = {
         x: center_x,
         y: center_y,
-        r: radius,
+        r: this.img.width,
         s_angle: 0,
         e_angle: 2 * Math.PI
     }
@@ -66,21 +70,11 @@ function Drum(drum_name, sound_src, center_x, center_y, radius) {
     this.draw = function() {
         var canvas = document.getElementById("drumCanvas");
         var ctx = canvas.getContext("2d");
-
-        ctx.fillStyle = "yellow";
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-
         if(this.is_playing) {
-            var radius = this.arc.r * 1.1;
+            ctx.drawImage(this.img, this.arc.x, this.arc.y, this.img.width * 1.1, this.img.height * 1.1);
         } else {
-            var radius = this.arc.r;
+            ctx.drawImage(this.img, this.arc.x, this.arc.y, this.img.width, this.img.height);
         }
-        ctx.arc(this.arc.x, this.arc.y, radius, this.arc.s_angle, this.arc.e_angle);
-        ctx.stroke();
-        ctx.fill();
-        ctx.closePath();
     }
 
     this.play = function() {
@@ -96,8 +90,11 @@ function Drum(drum_name, sound_src, center_x, center_y, radius) {
     }
 
     this.isInBounds = function(x, y) {
-        var coord_radius = Math.sqrt(Math.pow(x - this.arc.x, 2) + Math.pow(y - this.arc.y, 2));
-        if(coord_radius <= this.arc.r) {
+        var x_lower = this.arc.x;
+        var x_upper = this.arc.x + this.img.width;
+        var y_lower = this.arc.y;
+        var y_upper = this.arc.y + this.img.height;
+        if(x > x_lower && x < x_upper && y > y_lower && y < y_upper) {
             return true;
         } else {
             return false;
