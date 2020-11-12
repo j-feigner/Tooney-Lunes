4b1293964a92c53795cffe400758298d868c293b
bac18 <54698108+bac18@users.noreply.github.com>
2020-11-11 18:18:55 -0800

    instrument switch buttons placement, piano radio buttons redesigned

--- src/style.js
+++ src/style.js
@@ -1,12 +1,12 @@
 function profileDetailsSlideIn() {
     var username_container = document.getElementById("usernameContainer");
-    var email_container    = document.getElementById("emailContainer");
+    var email_container = document.getElementById("emailContainer");
 
     var username_width = username_container.offsetWidth;
-    var email_width    = email_container.offsetWidth;
+    var email_width = email_container.offsetWidth;
 
     username_container.style.borderBottomRightRadius = "20px";
-    email_container.style.borderBottomRightRadius    = "20px";
+    email_container.style.borderBottomRightRadius = "20px";
 
     if (username_width >= "288")
         username_container.style.borderTopRightRadius = "20px";
