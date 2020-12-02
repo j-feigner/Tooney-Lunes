d0a98d9e80b4931b67659561bbebf2a5824d185b
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-12-02 08:29:00 -0800

    Create event listeners to change internal chord values

--- src/www/js/chords.js
+++ src/www/js/chords.js
@@ -13,12 +13,15 @@ function main() {
 
     var chords = [];
 
+    // Grab all HTML element references and store in Chord objects
     for(var i = 0; i < chords_list.length; i++) {
         var chord_display = chords_list[i];
         chords[i] = new Chord();
         chords[i].root_selector_element = chord_display.querySelector(".chord-root-selection");
         chords[i].mode_selector_element = chord_display.querySelector(".chord-mode-selection");
         chords[i].display_element = chord_display.querySelector(".chord-label");
+        chords[i].update();
+        chords[i].createEventListeners();
     }
 
     var stopper = 0;
@@ -32,4 +35,20 @@ function Chord() {
     this.root_selector_element = null;
     this.mode_selector_element = null;
     this.display_element = null;
+
+    this.createEventListeners = function() {
+        this.root_selector_element.addEventListener("change", () => {
+            this.root = this.root_selector_element.selectedIndex;
+        })
+
+        this.mode_selector_element.addEventListener("change", () => {
+            this.mode = this.mode_selector_element.value;
+        })
+    }
+
+    this.update = function() {
+        this.root = this.root_selector_element.selectedIndex;
+        this.mode = this.mode_selector_element.value;
+        this.display = this.display_element.innerHTML;
+    }
 }
