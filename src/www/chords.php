66a885bff54f0ec595f0f4c18094df6559dd0453
Jordan Feigner <jordan.feigner.r@gmail.com>
2020-12-02 08:22:58 -0800

    Chord Progression Generator template

--- src/www/chords.php
+++ src/www/chords.php
@@ -5,29 +5,120 @@
         $header = file_get_contents("html/header.html");
         echo $header;
     ?>
+    <link rel="stylesheet" href="css/chords_style.css">
 </head>
 <body>
     <?php
-        $top_bar = file_get_contents("html/top_bar.html");
+        $top_bar = file_get_contents("html/tooney_title_top_bar.html");
         echo $top_bar;
     ?>
 
     <div id="pageContents" class="page-contents">
-        <div id="chordContainer" class="canvas-container">
-            <span>CHORD I</span>
-            <span>CHORD II</span>
-            <span>CHORD III</span>
-            <span>CHORD IV</span>
-
-            <script src="js/chords.js"></script>
-            <script src="js/utils.js"></script>
+        <div class="chord-app-container">
+            <div class="chords-container">
+                <div class="chord-display-container">
+                    <span class="chord-label">CHORD I</span>
+                    <div class="chord-selection">
+                        <select class="chord-root-selection">
+                            <option>C</option>
+                            <option>C#</option>
+                            <option>D</option>
+                            <option>D#</option>
+                            <option>E</option>
+                            <option>F</option>
+                            <option>F#</option>
+                            <option>G</option>
+                            <option>G#</option>
+                            <option>A</option>
+                            <option>A#</option>
+                            <option>B</option>
+                        </select>
+                        <select class="chord-mode-selection">
+                            <option>Major</option>
+                            <option>Minor</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="chord-display-container">
+                    <span class="chord-label">CHORD II</span>
+                    <div class="chord-selection">
+                        <select class="chord-root-selection">
+                            <option>C</option>
+                            <option>C#</option>
+                            <option>D</option>
+                            <option>D#</option>
+                            <option>E</option>
+                            <option>F</option>
+                            <option>F#</option>
+                            <option>G</option>
+                            <option>G#</option>
+                            <option>A</option>
+                            <option>A#</option>
+                            <option>B</option>
+                        </select>
+                        <select class="chord-mode-selection">
+                            <option>Major</option>
+                            <option>Minor</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="chord-display-container">
+                    <span class="chord-label">CHORD III</span>
+                    <div class="chord-selection">
+                        <select class="chord-root-selection">
+                            <option>C</option>
+                            <option>C#</option>
+                            <option>D</option>
+                            <option>D#</option>
+                            <option>E</option>
+                            <option>F</option>
+                            <option>F#</option>
+                            <option>G</option>
+                            <option>G#</option>
+                            <option>A</option>
+                            <option>A#</option>
+                            <option>B</option>
+                        </select>
+                        <select class="chord-mode-selection">
+                            <option>Major</option>
+                            <option>Minor</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="chord-display-container">
+                    <span class="chord-label">CHORD IV</span>
+                    <div class="chord-selection">
+                        <select class="chord-root-selection">
+                            <option>C</option>
+                            <option>C#</option>
+                            <option>D</option>
+                            <option>D#</option>
+                            <option>E</option>
+                            <option>F</option>
+                            <option>F#</option>
+                            <option>G</option>
+                            <option>G#</option>
+                            <option>A</option>
+                            <option>A#</option>
+                            <option>B</option>
+                        </select>
+                        <select class="chord-mode-selection">
+                            <option>Major</option>
+                            <option>Minor</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="chords-controls">
+                <button type="button" class="start-button">Start</button>
+                <div class="tempo-container">
+                    <span class="tempo-label">Tempo</span>
+                    <input type="range" min="60" max="240" step="1" value="120">
+                </div>
+            </div>
         </div>
-    </div>
-
-    <div id="bottomBar" class="bottom-bar">
-    <button type="button" id="loopChords" class="menu-button">Start</button>
-        <input type="range" id="songTempo" min="60" max="200" step="1" value="120">
-        <span id="tempoValue"></span>
+        <script src="js/chords.js"></script>
+        <script src="js/utils.js"></script>
     </div>
 </body>
 </html>
--- src/www/js/chords.js
+++ src/www/js/chords.js
@@ -1,71 +1,35 @@
 window.onload = main;
 
 function main() {
-    var url_header = "sounds/piano/";
+    var piano_srcs = [];
 
-    var piano_srcs = [
-        url_header + "C_3.mp3",
-        url_header + "Cs_3.mp3",
-        url_header + "D_3.mp3",
-        url_header + "Ds_3.mp3",
-        url_header + "E_3.mp3",
-        url_header + "F_3.mp3",
-        url_header + "Fs_3.mp3",
-        url_header + "G_3.mp3",
-        url_header + "Gs_3.mp3",
-        url_header + "A_3.mp3",
-        url_header + "As_3.mp3",
-        url_header + "B_3.mp3",
-        url_header + "C_4.mp3",
-        url_header + "Cs_4.mp3",
-        url_header + "D_4.mp3",
-        url_header + "Ds_4.mp3",
-        url_header + "E_4.mp3",
-        url_header + "F_4.mp3",
-        url_header + "Fs_4.mp3",
-        url_header + "G_4.mp3",
-        url_header + "Gs_4.mp3",
-        url_header + "A_4.mp3",
-        url_header + "As_4.mp3",
-        url_header + "B_4.mp3",
-        url_header + "C_5.mp3",
-        url_header + "Cs_5.mp3",
-        url_header + "D_5.mp3",
-        url_header + "Ds_5.mp3",
-        url_header + "E_5.mp3",
-        url_header + "F_5.mp3",
-        url_header + "Fs_5.mp3",
-        url_header + "G_5.mp3",
-        url_header + "Gs_5.mp3",
-        url_header + "A_5.mp3",
-        url_header + "As_5.mp3",
-        url_header + "B_5.mp3",
-        url_header + "C_6.mp3"
-    ];
+    loadInstrument("piano", piano_srcs, function() {
+        //alert("Sounds loaded");
+    });
 
-    var audio_ctx = new AudioContext();
-    var sounds = [];
+    var app_container = document.querySelector(".chord-app-container");
+    var chords_container = app_container.querySelector(".chords-container");
+    var chords_list = chords_container.querySelectorAll(".chord-display-container");
 
-    var req_remaining = piano_srcs.length;
-    piano_srcs.forEach((note, index) => {
-        var req = new XMLHttpRequest();
-        req.open("GET", note);
-        req.responseType = "arraybuffer";
-        req.onload = function() {
-            var buffer_src = req.response;
-            audio_ctx.decodeAudioData(buffer_src, 
-                function(buffer) { // Decoding success callback
-                    sounds[index] = buffer;
-                    req_remaining--;
-                    if(req_remaining === 0) {
-                        alert("All resources loaded.");
-                    }
-                }, 
-                function(error) { // Decoding failure callback
-                    alert(error);
-                }
-            );
-        }
-        req.send();
-    })
+    var chords = [];
+
+    for(var i = 0; i < chords_list.length; i++) {
+        var chord_display = chords_list[i];
+        chords[i] = new Chord();
+        chords[i].root_selector_element = chord_display.querySelector(".chord-root-selection");
+        chords[i].mode_selector_element = chord_display.querySelector(".chord-mode-selection");
+        chords[i].display_element = chord_display.querySelector(".chord-label");
+    }
+
+    var stopper = 0;
+}
+
+function Chord() {
+    this.root = "";
+    this.mode = "";
+    this.display = "";
+
+    this.root_selector_element = null;
+    this.mode_selector_element = null;
+    this.display_element = null;
 }
